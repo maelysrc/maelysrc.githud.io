2dfcc302aea2374dfdd1fb85c40b52f08aba7c99
maelysrc <60781988+maelysrc@users.noreply.github.com>
2020-05-30 23:18:50 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+ // Wrap every letter in a span
+
+ 
+var textWrapper = document.querySelector('.ml6 .letters');
+textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
+
+anime.timeline({loop: 1})
+  .add({
+    targets: '.ml6 .letter',
+    translateY: ["1.1em", 0],
+    translateZ: 0,
+    duration: 750,
+    delay: (el, i) => 50 * i
+  })
